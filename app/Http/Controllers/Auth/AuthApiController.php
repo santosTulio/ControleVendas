35e6389396e687ed9626a4f36cd44d53f4730cc5
Getulio dos Santos <getulio.santos@uft.edu.br>
2021-12-05 21:34:43 -0300

    [ADD] Cria controller para login pela API

---
+++ app/Http/Controllers/Auth/AuthApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthApiController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
